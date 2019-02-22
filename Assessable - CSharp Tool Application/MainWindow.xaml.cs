5c9cef782c89eb8c86f3ce97c3622d5b6b8d2cac
Alexis Serna <7319aserna@gmail.com>
2019-02-21 17:42:39 -0800

    Assessable - CSharp Tool Application (Created WPF)

---
+++ Assessable - CSharp Tool Application/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assessable_CSharp_Tool_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // The following code below is implemented in 
+            // the XAML window, but is also implemented
+            // below but commented out for future reference.
+
+            // (Lock Window Size)
+            ResizeMode = ResizeMode.NoResize;
+
+            InitializeComponent();
+        }
+
+        private void Game_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(@"C:\Users\s189074\Downloads\My Name Is Jeff Sound Effect.mp3"));
+            mediaPlayer.Volume = 1;
+            mediaPlayer.Play();
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(@"C:\Users\s189074\Downloads\Misc\Persona 4 Ending Theme - Never More (+ English LyricsSubs).mp3"));
+            mediaPlayer.Volume = 1;
+            mediaPlayer.Play();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            jd.Background = Brushes.Black;
+        }
+    }
+}
